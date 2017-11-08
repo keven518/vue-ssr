8570ddb168b3db368acffa6200070e40145e36a7
柯文 <kewen@kewendeiMac.local>
2017-11-08 21:47:53 +0800

    init

---
+++ server.js
@@ -0,0 +1,30 @@
+const Vue = require('vue')
+const server = require('express')()
+const renderer = require('vue-server-renderer').createRenderer({
+  template: require('fs').readFileSync('./src/index.template.html', 'utf-8')
+})
+server.get('*', (req, res) => {
+  const app = new Vue({
+    data: {
+      url: req.url
+    },
+    template: `<div>访问的 URL 是： {{ url }}</div>`
+  })
+
+  const context = {
+    title: "vue ssr app",
+    meta: `
+      <meta ...>
+      <meta ...>
+    `
+  }
+
+  renderer.renderToString(app, context, (err, html) => {
+    if (err) {
+      res.status(500).end('Internal Server Error')
+      return
+    }
+    res.end(html)
+  })
+})
+server.listen(8080)
